6b2adf760b639f4122d3ac0ffc40986f4c1f94af
Alumno Pedro <alumno.187277@ies-azarquiel.es>
2023-05-27 11:05:17 +0200

    Validacion en formulario registro

--- js/registro.js
+++ js/registro.js
@@ -1,41 +1,75 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function() {
-    // on ready
+$(document).ready(function () {
+  // on ready
 });
 
-async function registrarSocio(){
-let datos = {};
-datos.id = new Date().getTime();
-datos.nombre = document.getElementById('nombre').value;
-datos.apellido = document.getElementById('apellidos').value;
-datos.email = document.getElementById('email').value;
-datos.password = document.getElementById('pass').value;
-let repetirPassword = document.getElementById('repPass').value;
-if(repetirPassword != datos.password){
-  Swal.fire(
-    'Error',
-    'Las contraseñas no coinciden',
-    'error'
-  )     
-  return;
-}
+async function registrarSocio() {
+  let datos = {};
+  datos.id = new Date().getTime();
+  datos.nombre = document.getElementById('nombre').value;
+  datos.apellido = document.getElementById('apellidos').value;
+  datos.email = document.getElementById('email').value;
+  datos.password = document.getElementById('pass').value;
+  let repetirPassword = document.getElementById('repPass').value;
+  let valido = true;
+  if (!datos.nombre) {
+    document.querySelector(".error-nombre").textContent = "El nombre no puede estar vacio";
+    valido = false;
+  } else {
+    document.querySelector(".error-nombre").textContent = "";
+    valido = true;
+  }
+  if (!datos.apellido) {
+    document.querySelector(".error-apellidos").textContent = "El apellido no puede estar vacio";
+    valido = false;
+  } else {
+    document.querySelector(".error-nombre").textContent = "";
+    valido = true;
+  }
+  if (!datos.email) {
+    document.querySelector(".error-email").textContent = "El email no puede estar vacio";
+    valido = false;
+  } else {
+    document.querySelector(".error-nombre").textContent = "";
+    valido = true;
+  }
+  if (!datos.password) {
+    document.querySelector(".error-contraseña").textContent = "La contraseña no puede estar vacia";
+    valido = false;
+  } else {
+    document.querySelector(".error-nombre").textContent = "";
+    valido = true;
+  }
 
-  const request = await fetch('http://localhost:8080/api/registro', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(datos)
-  }).then(response => {
-    console.log(response);
-    return response.json();
-  })
-    .then(data => {
-      console.log(data);
-      window.location.href = "login.html";
+  if (repetirPassword != datos.password) {
+    Swal.fire(
+      'Error',
+      'Las contraseñas no coinciden',
+      'error'
+    )
+    valido = false;
+  } else {
+    valido = true;
+  }
+
+  if (valido) {
+    const request = await fetch('http://localhost:8080/api/registro', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(datos)
+    }).then(response => {
+      console.log(response);
+      return response.json();
     })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+      .then(data => {
+        console.log(data);
+        window.location.href = "login.html";
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
 }
